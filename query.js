4f51cffcda70432aaba7cfdd9a6104867592624f
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-12 11:59:55 +0200

    added a route that returns a random exercise with testcases

--- query.js
+++ query.js
@@ -1,8 +1,9 @@
 const User = require("./models").user;
 const Exercise = require("./models").exercise;
+const TestCase = require("./models").testCase;
 
 const getUsers = async () => {
-  const users = await User.findAll({ include: [Exercise] });
+  const users = await Exercise.findAll({ include: [TestCase] });
   console.log(users.map((u) => u.get({ plain: true })));
 };
 
