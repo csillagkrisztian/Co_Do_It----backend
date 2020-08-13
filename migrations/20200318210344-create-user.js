c23ef14d25b51fe08b00ab6c4f945cd7897a862c
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-13 11:07:46 +0200

    added new column to the user table and updated signup endpoint

--- migrations/20200318210344-create-user.js
+++ migrations/20200318210344-create-user.js
@@ -6,32 +6,36 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       email: {
         type: Sequelize.STRING,
         unique: true,
-        allowNull: false
+        allowNull: false,
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+      },
+      accountType: {
+        type: Sequelize.ENUM("teacher", "guest", "student", "admin"),
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("users");
-  }
+  },
 };
