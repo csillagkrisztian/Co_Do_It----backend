61c26b7349e18ec0fd5b6664f1c177b6a5db422e
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-20 00:19:52 +0200

    final commit for the profiles

--- routers/teachers.js
+++ routers/teachers.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models").user;
+const authMiddleware = require("../auth/middleware");
 
-router.get("/", async (req, res, next) => {
+router.get("/", authMiddleware, async (req, res, next) => {
   try {
     const teachers = await User.findAll({ where: { accountType: "teacher" } });
     const teacherNames = teachers.map((t) => t.dataValues.name);
